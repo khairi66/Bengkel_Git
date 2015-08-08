8f22ce8100cfe9aa95ea2a7b5ff5ab00389a9f7c
imaprog <shafiqaziz06@gmail.com>
2015-08-08 10:21:52 +0800

    my first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head><h1>Resume</h1></head>
+    <body>
+        <ul>
+            <li>Mohamad Shafiq Bin Aziz</li>
+            <li>920311065197</li>
+            <li>Developer</li>
+        </ul>
+    </body>
+</html>
