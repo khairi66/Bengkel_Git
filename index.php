fe70ab886e558f88352883ae462a79437d9d362e
khairi66 <khairizunaini@gmail.com>
2015-08-08 14:52:28 +0800

    add collaborator jer

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <html>
 <head><h1>Selamat Datang ke Bengkel Git</h1></head>
     <body>
-        <ul>
+        <ul> Leader Collaborator
             <li>Mohamad Shafiq Bin Aziz</li>
             <li>920311065197</li>
             <li>Developer</li>
             <li>Unix Lover</li>
         </ul>
+
+        <ul> Co-Leader Collaborator
+        	<li>Khairi Zunaini</li>
+            <li>930520035161</li>
+            <li>Businessman</li>
+            <li>Durian Lover</li>
         <ul>
+
         <ol>####changed made from master branch####</ol>
+        <ol>
         </ul>
     </body>
 </html>
